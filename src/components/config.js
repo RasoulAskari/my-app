060305cf58a5d1bfd0a05dac8592d0089fde0f19
RasoulDebugger <rasoul.merzai123@gmail.com>
2023-05-22 18:24:51 +0500

    delete test and change config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,20 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_oHoaRePXGjf-Gdvbc25hib63O_-512M",
-  authDomain: "my-app-ad3ae.firebaseapp.com",
-  projectId: "my-app-ad3ae",
-  storageBucket: "my-app-ad3ae.appspot.com",
-  messagingSenderId: "213473220493",
-  appId: "1:213473220493:web:658e62a25f6096094d0fc4",
+  apiKey: "AIzaSyDTMCtkSD-G6czBl12pExtHJ62uue459_M",
+  authDomain: "my-next-app-405c7.firebaseapp.com",
+  projectId: "my-next-app-405c7",
+  storageBucket: "my-next-app-405c7.appspot.com",
+  messagingSenderId: "278654166863",
+  appId: "1:278654166863:web:e7b129ffe8e559aee7073a",
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
+const app = initializeApp(firebaseConfig);
